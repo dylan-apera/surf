1ef87cd6064001c847b5818b7754cc1afaa28e55
dylan-apera <74391707+dylan-apera@users.noreply.github.com>
2021-01-13 10:03:10 +1300

    adjustment to strcuture

---
+++ weather-forecast-api/Controllers/weather.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace weather_forecast_api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Weather : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var weatherForecast = new[]
+            {
+                new { breakName = "Muriawai", highTide = "12:30AM & 11:00 AM", lowTide = "6:30AM & 5:PM", wind = "North North East Winds @ 3 knots", swell = "1.2M from the West"},
+                new { breakName = "Hamilton", highTide = "12:30AM & 11:00 AM", lowTide = "6:30AM & 5:PM", wind = "North North East Winds @ 3 knots", swell = "1.2M from the West"}
+            };
+            return new JsonResult(weatherForecast);
+        }            
+    }
+}
+/*
+
+    [Route("[controller]")]
+    [ApiController]
+    public class Reviews : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var surfReviews = new[]
+            {
+                new { surfer= "Dan Larsen", comment = "This spot is sick! "}
+            };
+
+            return new JsonResult(surfReviews);
+        }
+
+
+    public class person
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+    }
+    [HttpPost]
+    public void Post([FromBody] person p)
+    {
+        return new JsonResult(person.p);
+    }
+
+}*/
+
+
