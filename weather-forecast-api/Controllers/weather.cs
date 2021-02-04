222af2c349b945a4dd4290ab9dcaae78d901468a
Dylan Apera <dylanapera@microsoft.com>
2021-02-04 22:51:56 +1300

    commit changes from older laptop

--- weather-forecast-api/Controllers/weather.cs
+++ weather-forecast-api/Controllers/weather.cs
@@ -1,29 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
+//Todo GET a specific value - homework
 namespace weather_forecast_api.Controllers
 {
     [Route("[controller]")]
     [ApiController]
     public class Weather : ControllerBase
     {
+        private static ConcurrentDictionary <string, Surf> _data = new ConcurrentDictionary <string, Surf> ();
+
         // GET: api/<ValuesController>
         [HttpGet]
         public IActionResult Get()
         {
-            var weatherForecast = new[]
+            /*var weatherForecast = new[]
             {
                 new { breakName = "Muriawai", highTide = "12:30AM & 11:00 AM", lowTide = "6:30AM & 5:PM", wind = "North North East Winds @ 3 knots", swell = "1.2M from the West"},
                 new { breakName = "Hamilton", highTide = "12:30AM & 11:00 AM", lowTide = "6:30AM & 5:PM", wind = "North North East Winds @ 3 knots", swell = "1.2M from the West"},
                 new { breakName = "Social Center", highTide = "12:30AM & 11:00 AM", lowTide = "6:30AM & 5:PM", wind = "East Winds @ 10 knots", swell = "1.2M from the West"}
             };
-            return new JsonResult(weatherForecast);
+            return new JsonResult(weatherForecast);*/
+
+            return new JsonResult(_data);
         }            
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Surf surf)
+        {
+            _data.TryAdd(surf.Id, surf);
+            return Ok();
+        }
     }
 }
 /*
@@ -57,5 +71,7 @@ namespace weather_forecast_api.Controllers
     }
 
 }*/
+
+//Todo GET a specific value - homework
 
 
