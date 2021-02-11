3dd058886178d10249b79c4e8b15e5862e8be1c0
Dylan Apera <dylan.n.apera@hotmail.com>
2021-02-11 14:51:13 +1300

    ps database script cleaned out

--- deploy/deploy-az-db.ps1
+++ deploy/deploy-az-db.ps1
@@ -7,8 +7,8 @@ $sqldb = 'weather-sql-db'
 $login="dylanapera"
 $password="c1sc0Dylan!"
 
-$startIP=10.0.0.0
-$endIP=10.0.0.254
+$startIP = 10.0.0.0
+$endIP = 10.0.0.254
 
 echo "Creating $resource..."
 az group create --name $resource --location "$location"
@@ -17,7 +17,7 @@ echo "Creating $sqlserver in $location..."
 az sql server create --name $sqlserver --resource-group $resource --location $location --admin-user $login --admin-password $password
 
 echo "Configuring firewall..."
-az sql server firewall-rule create --resource-group $resource --server $sqlserver -n AllowYourIp --start-ip-address $startIP --end-ip-address $endIP
+az sql server firewall-rule create --resource-group $resource --server $sqlserver -n AllowYourIp --start-ip-address 10.0.0.1 --end-ip-address 10.0.0.5
 
 echo "Creating $sqldb on $sqlserver..."
-az sql db create --resource-group $resource --server $sqlserver --name $sqldb --edition GeneralPurpose --family Gen4 --capacity 1 --zone-redundant false # zone redundancy is only supported on premium and business critical service tiers
+az sql db create --resource-group $resource --server $sqlserver --name $sqldb --edition Standard --family Gen5 --service-objective S2 --zone-redundant false # zone redundancy is only supported on premium and business critical service tiers
